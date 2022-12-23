a445b9e1a2175e75e0c5d0a4815400f74f82f8ac
king lich <iljapuskov@gmail.com>
2022-12-24 00:03:37 +0300

    implement identity server

---
+++ IdentityService/IdentityServerSettings/IdentityServerDbInitializer.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.IdentityServerSettings;
+
+public static class IdentityServerDbInitializer
+{
+    public static void InitializeDatabase(this IApplicationBuilder app)
+    {
+        using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope();
+        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+        if (!context.Clients.Any())
+        {
+            foreach (var client in IdentityServerConfiguration.GetClients())
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.IdentityResources.Any())
+        {
+            foreach (var resource in IdentityServerConfiguration.GetIdentityResources())
+            {
+                context.IdentityResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+
+        if (!context.ApiScopes.Any())
+        {
+            foreach (var resource in IdentityServerConfiguration.GetApiScopes())
+            {
+                context.ApiScopes.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+        if (!context.ApiResources.Any())
+        {
+            foreach (var resource in IdentityServerConfiguration.GetApiResources())
+            {
+                context.ApiResources.Add(resource.ToEntity());
+            }
+            context.SaveChanges();
+        }
+    }
+}
